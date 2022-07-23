b267210d691369245d25e3061c40bf4269d8bb81
thanhthai3011 <55083667+thanhthai3011@users.noreply.github.com>
2022-07-23 23:18:33 +0700

    change index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,18 +14,18 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <a href="https://nextjs.org">This is the first lesson nextjs</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          I will try{' '}
+          <code className={styles.code}>hello may fen</code>
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <p>Ngo Thanh Thai</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
